4c98f22414b5053e41919f1f0ba89471d7e3c661
Gabriel Haugbøl <grh@it-minds.dk>
2022-12-29 13:19:15 +0100

    Add nested routing.

---
+++ dev-server/routes/some-resource/index.js
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const someResourceRouter = express.Router();
+
+someResourceRouter.get('/some-resources', (req, res) => {
+        res.status(200);
+        res.send("Getting all of 'some resource'");
+    }
+);
+
+export {
+    someResourceRouter,
+}
