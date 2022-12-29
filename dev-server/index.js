912d62e7bdeb3a6049795aadce406e15ef00bac4
Gabriel Haugbøl <grh@it-minds.dk>
2022-12-29 01:13:13 +0100

    Add Initial Setup Of Express Server

--- dev-server/index.js
+++ dev-server/index.js
@@ -1,1 +1,15 @@
-console.log("Server running");
+/* eslint-disable no-undef */
+import express from "express";
+import * as dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+
+app.listen(process.env.EXPRESS_PORT, (error) => {
+    if (!error) {
+        console.log(`Server is Successfully Running on Port ${process.env.EXPRESS_PORT}`);
+    } else {
+        console.log("Error occurred, server can't start", error);
+    }
+}
+);
