fbca9b596c7627da0633c97cc121c62f55fa8dfd
Gabriel Haugbøl <grh@it-minds.dk>
2022-12-29 19:07:24 +0100

    Add vuetify

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,15 @@ import vue from "@vitejs/plugin-vue";
 
 dotenv.config();
 
+// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
+import vuetify from 'vite-plugin-vuetify'
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+		vue(),
+		vuetify({ autoImport: true }),
+	],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
