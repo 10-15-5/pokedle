5d16e3406b7b072e4904bc4c1a320df2e8f38a6a
Gabriel Haugbøl <grh@it-minds.dk>
2023-01-16 21:21:22 +0100

    Add progress on json file parsing.

--- dev-server/services/guess.go
+++ dev-server/services/guess.go
@@ -3,9 +3,8 @@ package services
 import (
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
+	"log"
 	"os"
-	"strconv"
 )
 
 type GuessState string
@@ -17,19 +16,24 @@ const (
 	WrongGuess                    = "WrongGuess"
 )
 
-type Pokemons struct {
-	Pokemons []Pokemon `json:"pokemons"`
-}
-
 type Pokemon struct {
-	name           string `json:"name"`
-	type1          string `json:"type1"`
-	type2          string `json:"type2"`
-	generation     int    `json:"generation"`
-	evolutionState int    `json:"evolutionState"`
-	isFullyEvolved string `json:"isFullyEvolved"`
+	Number         int    `json:"number"`
+	Name           string `json:"name"`
+	Type1          string `json:"type1"`
+	Type2          string `json:"type2"`
+	Total          int    `json:"total"`
+	Hp             int    `json:"hp"`
+	Attack         int    `json:"attack"`
+	Defense        int    `json:"defense"`
+	SpAtk          int    `json:"sp-atk"`
+	SpDef          int    `json:"sp-def"`
+	Speed          int    `json:"speed"`
+	Generation     int    `json:"generation"`
+	Legendary      string `json:"legendary"`
+	Evolutions     string `json:"evolutions"`
+	EvolutionState int    `json:"evolutionState"`
+	IsFullyEvolved string `json:"isFullyEvolved"`
 }
-
 type GuessResult struct {
 	name           GuessState `json:"name"`
 	type1          GuessState `json:"type1"`
@@ -42,27 +46,25 @@ type GuessResult struct {
 func SubmitGuess(name string) {
 	fmt.Printf("Name passed to SubmitGuess: %v\n", name)
 
-	jsonFile, err := os.Open("./data/pokemonData-v2.json")
+	content, err := os.ReadFile("./data/pokemonData-v2.json")
 
 	if err != nil {
-		fmt.Println(err)
+		log.Fatal("Error when opening file: ", err)
 	}
 
 	fmt.Println("Successfully Opened jsonFile")
 
-	defer jsonFile.Close()
+	var pokemons []Pokemon
 
-	fmt.Println(*jsonFile)
+	err = json.Unmarshal(content, &pokemons)
 
-	byteValue, _ := ioutil.ReadAll(jsonFile)
+	if err != nil {
+		log.Fatal("Error during Unmarshal(): ", err)
+	}
 
-	var pokemons Pokemons
-
-	json.Unmarshal(byteValue, &pokemons)
-
-	fmt.Println("Slice length: " + strconv.Itoa(len(pokemons.Pokemons)))
+	fmt.Printf("Pokemons : %+v\n", pokemons)
 
 	for i := 0; i < 10; i++ {
-		fmt.Println("Pokemon name: " + pokemons.Pokemons[i].name)
+		fmt.Printf("Pokemon name: %v\n", pokemons[i].name)
 	}
 }
