6eccf20de4cadafa2c96f0027792a3c567a4c29b
Gabriel Haugbøl <grh@it-minds.dk>
2023-01-17 07:55:15 +0100

    Format json data on load struct

--- dev-server/services/guessValidation.go
+++ dev-server/services/guessValidation.go
@@ -17,20 +17,10 @@ const (
 )
 
 type Pokemon struct {
-	Number         int    `json:"number"`
 	Name           string `json:"name"`
 	Type1          string `json:"type1"`
 	Type2          string `json:"type2"`
-	Total          int    `json:"total"`
-	Hp             int    `json:"hp"`
-	Attack         int    `json:"attack"`
-	Defense        int    `json:"defense"`
-	SpAtk          int    `json:"sp-atk"`
-	SpDef          int    `json:"sp-def"`
-	Speed          int    `json:"speed"`
 	Generation     int    `json:"generation"`
-	Legendary      string `json:"legendary"`
-	Evolutions     string `json:"evolutions"`
 	EvolutionState int    `json:"evolutionState"`
 	IsFullyEvolved string `json:"isFullyEvolved"`
 }
@@ -60,5 +50,10 @@ func SubmitGuess(name string) {
 
 	if err != nil {
 		log.Fatal("Error during Unmarshal(): ", err)
+	}
+
+	for i := 0; i < 10; i++ {
+		fmt.Printf("Pokemon name: %v\n", pokemons[i].Name)
+		fmt.Printf("Pokemon name: %v\n", pokemons[i])
 	}
 }
