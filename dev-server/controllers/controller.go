22e2b8b59d8ff0625541583c1caec1868c01b6ed
Gabriel Haugbøl <grh@it-minds.dk>
2023-01-25 06:43:59 +0100

    Comment out submitGuess

--- dev-server/controllers/controller.go
+++ dev-server/controllers/controller.go
@@ -8,16 +8,16 @@ import (
 )
 
 func SubmitGuess(c *gin.Context) {
-	var name string
-	println("Call enter")
+	// var name string
+	// println("Call enter")
 
-	if err := c.BindJSON(&name); err != nil {
-		return
-	}
+	// if err := c.BindJSON(&name); err != nil {
+	// 	return
+	// }
 
-	services.SubmitGuess(name)
+	// services.SubmitGuess(name)
 
-	c.IndentedJSON(http.StatusOK, name)
+	// c.IndentedJSON(http.StatusOK, name)
 }
 
 func GetSecretPokemon(c *gin.Context) {
@@ -30,4 +30,14 @@ func GetSecretPokemon(c *gin.Context) {
 	c.BindJSON(&secretPokemon)
 
 	c.IndentedJSON(http.StatusOK, secretPokemon)
+}
+
+func UpdateDailySecretPokemon(c *gin.Context) {
+	err := services.UpdateDailySecretPokemon()
+
+	if err != nil {
+		c.AbortWithStatus(500)
+	}
+
+	c.Status(200)
 }
