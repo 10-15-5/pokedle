8b9be3677e366e926c29498c477db42e0cc49c3d
Gabriel Haugbøl <grh@it-minds.dk>
2023-03-02 11:28:48 +0100

    Fix Tailwind Not Working

--- client/main.js
+++ client/main.js
@@ -4,7 +4,7 @@ import router from './router'
 import vuetify from './plugins/vuetify'
 import { createPinia } from 'pinia'
 import { loadFonts } from './plugins/webfontloader'
-
+import './style.css';
 loadFonts();
 
 createApp(App)
