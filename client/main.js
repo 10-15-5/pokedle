2ca1d92c05c9a2ef2fe5f5dd1f4798f942a2415c
Gabriel Haugbøl <grh@it-minds.dk>
2023-03-02 21:10:54 +0100

    Add PrimeVue

--- client/main.js
+++ client/main.js
@@ -5,10 +5,14 @@ import vuetify from './plugins/vuetify'
 import { createPinia } from 'pinia'
 import { loadFonts } from './plugins/webfontloader'
 import './style.css';
+import PrimeVue from 'primevue/config';
+
 loadFonts();
 
 createApp(App)
     .use(createPinia())
+    .use(PrimeVue)
+    //.component('Card', Card)
     .use(router)
     .use(vuetify)
     .mount('#app');
