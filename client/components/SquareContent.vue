92e67bb3422bce775779f9dcc1a44a3c70d4f6bb
Gabriel Haugbøl <grh@it-minds.dk>
2023-03-03 17:14:52 +0100

    Add Tailwind SquareContent

--- client/components/SquareContent.vue
+++ client/components/SquareContent.vue
@@ -1,10 +1,11 @@
 <template>
     <div>
         <div v-if="pokemon && color === 'shiny'"
-             class="star-container">
-            <v-icon class="star-icon-background"
+             class="flex justify-center items-center w-[22px] h-[22px] z-1 absolute
+                  mt-[-8px] ml-[46px]">
+            <v-icon class="text-black dark:!text-dark-border text-[25px] absolute z-10"
                     icon="mdi-star" />
-            <v-icon class="star-icon"
+            <v-icon class="text-yellow-500 text-[17px] absolute z-10"
                     icon="mdi-star" />
         </div>
         <VueFlip class="square-content"
@@ -13,7 +14,7 @@
                  width="60px"
                  height="60px">
             <template v-slot:front>
-                <v-card class="square-content-front border-black"
+                <div class="result-card"
                         :style="{ 'background-color': getColor }"
                         variant="outlined">
                     <img v-if="pokemon"
@@ -21,22 +22,21 @@
                          :src="'https://img.pokemondb.net/sprites/ruby-sapphire/' + color + '/' + pokemon + '.png'"
                          alt="pokemon sprite" />
                     <img v-if="habitat"
-                         class="pokemon-habitat border-black"
+                         class="pokemon-habitat border-light-border dark:!border-dark-border"
                          :src="getHabitatImage(habitat)"
                          :title="`${habitat}`"
                          :alt="`${habitat}`" />
                     <p v-else-if="guessResult"
-                       class="text-content">{{ guessText }}</p>
-                </v-card>
+                       class="pt-1 pl-[2px] font-pkmEmerald text-[16px] capitalize dark:text-dark-text dark:font-bold">{{ guessText }}</p>
+                </div>
             </template>
             <template v-slot:back>
-                <v-card class="square-content-back border-black"
+                <div class="result-card"
                         variant="outlined">
-                        <img
-                        class="pokemon-bg"
+                    <img class="pokemon-bg"
                          :src="'./client/assets/pokemon-cardback-pixel.png'"
                          alt="pokemon sprite" />
-                </v-card>
+                </div>
             </template>
         </VueFlip>
     </div>
@@ -48,6 +48,8 @@ import { guessState } from '../constants.js';
 import { computed, onBeforeMount, onMounted, ref } from 'vue';
 import { getHabitatImage } from '../services/assets.js';
 import { VueFlip } from 'vue-flip';
+import { useDark } from '@vueuse/core';
+const isDark = useDark()
 
 const props = defineProps({
     guessResult: String,
@@ -61,7 +63,7 @@ const props = defineProps({
 const isCardFaceDown = ref(true);
 
 onBeforeMount(() => {
-    if(props.flipDelay===undefined) isCardFaceDown.value = false;
+    if (props.flipDelay === undefined) isCardFaceDown.value = false;
 });
 
 onMounted(() => {
@@ -73,11 +75,11 @@ onMounted(() => {
 const getColor = computed(() => {
     switch (props.guessResult) {
         case guessState.CorrectGuess:
-            return "rgb(70, 217, 48)";
+            return isDark.value ? "#10b981" : "rgb(70, 217, 48)";
         case guessState.PartlyCorrectGuess:
-            return "rgb(237, 159, 43)";
+            return isDark.value ? "#fb923c" : "rgb(237, 159, 43)";
         case guessState.WrongGuess:
-            return "rgb(237, 59, 43)";
+            return isDark.value ? "#f87171" : "rgb(237, 59, 43)";
         default:
             return "transparent";
     }
@@ -96,63 +98,5 @@ const getColor = computed(() => {
     border-style: solid;
     border-radius: 60%;
     border-width: 2px;
-}
-
-.square-content-front,
-.square-content-back {
-    border-width: 2px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    position: absolute;
-    width: 100%;
-    height: 100%;
-    -webkit-backface-visibility: hidden;
-    /* Safari */
-    backface-visibility: hidden;
-}
-
-/* Style the back side */
-.square-content-back {
-    border-width: 2px;
-}
-
-.text-content {
-    padding: 4px 0px 0px 2px;
-}
-
-.star-container {
-    width: 22px;
-    height: 22px;
-    z-index: 1;
-    position: absolute;
-    margin: -8px 0px 0px 46px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-}
-
-.star-icon {
-    color: rgb(249, 194, 53);
-    z-index: 1;
-    position: absolute;
-    font-size: 17px;
-}
-
-.star-icon-background {
-    z-index: 1;
-    position: absolute;
-    font-size: 25px;
-}
-
-p {
-    font-family: pkmEmerald;
-    font-size: 16px;
-    text-transform: capitalize;
-}
-
-/* capitalize alt text*/
-img {
-    text-transform: capitalize;
 }
 </style>
