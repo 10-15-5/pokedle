0ec2881726cada66a91cc5731da39a9a417481ac
Gabriel Haugbøl <grh@it-minds.dk>
2023-03-03 13:20:05 +0100

    Refactor SquareContentHeader To Use Tailwind

--- client/components/SquareContentHeader.vue
+++ client/components/SquareContentHeader.vue
@@ -1,12 +1,15 @@
 <template>
-    <v-card class="content bg-white border-black"
-            variant="outlined">
+    <v-card class="card gap-x-2 py-1 pl-2 static">
         <p v-for="(fieldTitle, _, index) in guessFieldTitles"
            :key="fieldTitle"
            :value="fieldTitle"
-           class="square-content">{{ fieldTitle }} <v-card v-if="getGuessFieldTooltipsFromIndex(index)"
-                    class="tooltip"
-                    variant="outlined"> {{ getGuessFieldTooltipsFromIndex(index) }} </v-card>
+           class="w-[60px] h-[40px] flex justify-center items-center border-b-2
+                            border-light-border dark:border-dark-border
+                             font-pkmEmerald font-bold text-center group">{{ fieldTitle }}
+        <div v-if="getGuessFieldTooltipsFromIndex(index)"
+             class="card transition-all duration-150 scale-0 origin-top absolute
+                        group-hover:scale-100 z-1 mb-[100px] p-2 delay-500
+                "> {{ getGuessFieldTooltipsFromIndex(index) }} </div>
         </p>
     </v-card>
 </template>
@@ -18,51 +21,4 @@ import { guessFieldTitles, getGuessFieldTooltipsFromIndex } from '../constants.j
 </script>
 
 <style scoped>
-.content {
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
-    gap: 8px;
-    padding: 8px 0 8px 8px;
-    border-width: 2px;
-    position: static;
-
-}
-
-.square-content {
-    width: 60px;
-    height: 40px;
-    display: flex;
-    justify-content: center;
-    border-bottom: 2px solid;
-    align-items: center;
-}
-
-p {
-    font-size: 15px;
-    font-weight: bolder;
-    font-family: pkmEmerald;
-    text-align: center;
-}
-
-.tooltip {
-    opacity: 0;
-    text-align: center;
-    padding: 6px;
-    margin-top: -102px;
-    border-width: 2px;
-    position: absolute;
-    z-index: 1;
-    transition: opacity 0.3s;
-    visibility: hidden;
-}
-
-
-
-/* Show the tooltip text when you mouse over the tooltip container */
-.square-content:hover .tooltip {
-    visibility: visible;
-    opacity: 1;
-    transition-delay: 0.8s;
-}
 </style>
