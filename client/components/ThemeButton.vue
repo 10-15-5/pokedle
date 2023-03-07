dc9be55ddcebf5c009f90646df787ca71037c1b9
Gabriel Haugbøl <grh@it-minds.dk>
2023-03-07 09:23:19 +0100

    Update Theme On Theme Switch

--- client/components/ThemeButton.vue
+++ client/components/ThemeButton.vue
@@ -1,6 +1,6 @@
 <template>
     <button
-        class="absolute right-0 z-10 m-4 flex h-11 w-11 items-center justify-center rounded-full bg-black/30 text-[24px] text-white hover:bg-black/20 hover:!text-cyan-400 dark:bg-white/20 dark:text-[28px] dark:text-black hover:dark:bg-white/30 dark:hover:!text-amber-400"
+        class="absolute right-0 z-10 m-4 flex h-11 w-11 items-center justify-center rounded-full bg-black/30 text-[24px] text-white hover:bg-black/10 hover:!text-cyan-400 dark:bg-white/40 dark:text-[28px] dark:text-black hover:dark:bg-white/20 dark:hover:!text-amber-400"
         @click="toggleActive()"
     >
         <span v-if="isActive" class="pi pi-sun"></span>
