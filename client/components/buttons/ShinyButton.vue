0922ddc7d4f0943218a58b164997084a859a81ae
Gabriel Haugbøl <grh@it-minds.dk>
2023-03-08 21:07:17 +0100

    Change Shiny Mode Icon To A Star And Change Colors To Orange

--- client/components/buttons/ShinyButton.vue
+++ client/components/buttons/ShinyButton.vue
@@ -1,11 +1,13 @@
 <template>
-    <button
-        class="toggle-button group"
-        @click="store.toggleShiny()"
-    >
-        <span v-if="store.isShiny" class="pi pi-eye-slash
-        group-hover:text-red-600 dark:group-hover:text-red-300 "></span>
-        <span v-else class="pi pi-palette group-hover:text-light-emerald dark:group-hover:text-dark-emerald"></span>
+    <button class="toggle-button group" @click="store.toggleShiny()">
+        <span
+            v-if="store.isShiny"
+            class="pi pi-eye-slash group-hover:text-red-600 dark:group-hover:text-red-300"
+        ></span>
+        <span
+            v-else
+            class="pi pi-star group-hover:text-orange-400 dark:group-hover:text-orange-300"
+        ></span>
     </button>
 </template>
 
