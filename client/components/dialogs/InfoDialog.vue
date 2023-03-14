4e37585972168f110b878b9de3c81236de2877e4
Gabriel Haugbøl <grh@it-minds.dk>
2023-03-14 17:05:28 +0100

    Update Styling Size Rules Dialogs

--- client/components/dialogs/InfoDialog.vue
+++ client/components/dialogs/InfoDialog.vue
@@ -1,14 +1,14 @@
 <template>
     <BaseDialog :isVisible="isVisible" :onClose="onClose">
-        <h1 class="font-pkmEmerald text-[32px] uppercase">About</h1>
-        <p class="font-pkmEmerald text-[20px]">
-            Welcome to the world of POKEMéN! <br />
-            Every day a wild secret POKéMON will be hiding and its your job as a POKéMON TRAINER to
-            catch (guess) it!
-        </p>
-        <hr class="divider" />
-        <h1 class="font-pkmEmerald text-[32px] uppercase">How To Play</h1>
-        <div class="font-pkmEmerald text-[20px]">
+        <div class="max-w-[700px] font-pkmEmerald text-[20px]">
+            <h1 class="font-pkmEmerald text-[32px] uppercase">About</h1>
+            <p class="font-pkmEmerald text-[20px]">
+                Welcome to the world of POKEMéN! <br />
+                Every day a wild secret POKéMON will be hiding and its your job as a POKéMON TRAINER
+                to catch (guess) it!
+            </p>
+            <hr class="divider" />
+            <h1 class="font-pkmEmerald text-[32px] uppercase">How To Play</h1>
             <p class="my-2">
                 <b>1. Guess:</b> Guess any random POKEMéN <b>from gen 1-3</b>, since we currently
                 have no clues. We will guess <b>SQUIRTLE</b> here.
@@ -35,67 +35,68 @@
                 class="card mx-auto w-[600px]"
                 :src="'./client/assets/game-examples/game-example.png'"
             />
-        </div>
 
-        <hr class="divider" />
-        <h1 class="font-pkmEmerald text-[32px] uppercase">Habitats</h1>
-        <p class="font-pkmEmerald text-[20px]">
-            The Habitats used are all taken from
-            <a
-                class="link"
-                href="https://bulbapedia.bulbagarden.net/wiki/List_of_Pok%C3%A9mon_by_habitat"
-            >
-                Bulbapedia
-            </a>
-            and include:
-        </p>
-        <div class="grid grid-flow-col gap-1 p-2 sm:grid-flow-col sm:grid-rows-3">
-            <img :src="'./client/assets/habitats/cave.png'" title="cave" />
-            <img :src="'./client/assets/habitats/forest.png'" title="forest" />
-            <img :src="'./client/assets/habitats/grassland.png'" title="grassland" />
-            <img :src="'./client/assets/habitats/mountain.png'" title="mountain" />
-            <img :src="'./client/assets/habitats/rare.png'" title="rare" />
-            <img :src="'./client/assets/habitats/rough-terrain.png'" title="rough-terrain" />
-            <img :src="'./client/assets/habitats/sea.png'" title="sea" />
-            <img :src="'./client/assets/habitats/urban.png'" title="urban" />
-            <img :src="'./client/assets/habitats/waters-edge.png'" title="waters-edge" />
-        </div>
+            <hr class="divider" />
+            <h1 class="font-pkmEmerald text-[32px] uppercase">Habitats</h1>
+            <p class="font-pkmEmerald text-[20px]">
+                The Habitats used are all taken from
+                <a
+                    class="link"
+                    href="https://bulbapedia.bulbagarden.net/wiki/List_of_Pok%C3%A9mon_by_habitat"
+                >
+                    Bulbapedia
+                </a>
+                and include:
+            </p>
+            <div class="grid grid-flow-col gap-1 p-2 sm:grid-flow-col sm:grid-rows-3">
+                <img :src="'./client/assets/habitats/cave.png'" title="cave" />
+                <img :src="'./client/assets/habitats/forest.png'" title="forest" />
+                <img :src="'./client/assets/habitats/grassland.png'" title="grassland" />
+                <img :src="'./client/assets/habitats/mountain.png'" title="mountain" />
+                <img :src="'./client/assets/habitats/rare.png'" title="rare" />
+                <img :src="'./client/assets/habitats/rough-terrain.png'" title="rough-terrain" />
+                <img :src="'./client/assets/habitats/sea.png'" title="sea" />
+                <img :src="'./client/assets/habitats/urban.png'" title="urban" />
+                <img :src="'./client/assets/habitats/waters-edge.png'" title="waters-edge" />
+            </div>
 
-        <hr class="divider" />
-        <h1 class="font-pkmEmerald text-[32px] uppercase">Credit</h1>
-        <div class="w-full font-pkmEmerald text-[20px] leading-8">
-            <p class="mb-2">
-                Pokédle is created by Gabriel Haugbøl. I hope you enjoy playing along 😊🎮!
+            <hr class="divider" />
+            <h1 class="font-pkmEmerald text-[32px] uppercase">Credit</h1>
+            <div class="w-full font-pkmEmerald text-[20px] leading-8">
+                <p class="mb-2">
+                    Pokédle is created by Gabriel Haugbøl. I hope you enjoy playing along 😊🎮!
+                </p>
+                <p class="mb-2">
+                    Pokédle is heavily inspired by
+                    <a class="link" href="https://loldle.net/">loldle.net</a> and
+                    <a href="https://www.nytimes.com/games/wordle/index.html" class="link">wordle</a
+                    >.
+                </p>
+                <p>
+                    <span class="font-extrabold">Background artists</span>
+                    <br />
+                    <b>- Gen 1-3 Collage black/white bg:</b>
+                    u/ZaryXYZ (reddit)
+                    <br />
+                    <b> - Cubone Background:</b> u/blizard72 (reddit)
+                    <br />
+                    <b>- Mt. Silver Battle Background: </b>
+                    - Mt. Silver Battle Background:
+                    <a class="link" href="https://www.deviantart.com/serain">Serian</a>
+                </p>
+                <p>Original artist/creator of image(s), please DM for removal or credit.</p>
+            </div>
+            <hr class="divider" />
+            <h1 class="text-center font-pkmEmerald text-[32px] uppercase">Contact</h1>
+            <p class="font-pkmEmerald text-[20px]">
+                If you have any questions/feedback/bug reports feel free to contact me on the email
+                below: <br />
+                &#8594;
+                <a class="link ml-2 text-[24px] font-bold" href="mailto: pokedle.contact@gmail.com">
+                    pokedle.contact@gmail.com
+                </a>
             </p>
-            <p class="mb-2">
-                Pokédle is heavily inspired by
-                <a class="link" href="https://loldle.net/">loldle.net</a> and
-                <a href="https://www.nytimes.com/games/wordle/index.html" class="link">wordle</a>.
-            </p>
-            <p>
-                <span class="font-extrabold">Background artists</span>
-                <br />
-                <b>- Gen 1-3 Collage black/white bg:</b>
-                u/ZaryXYZ (reddit)
-                <br />
-                <b> - Cubone Background:</b> u/blizard72 (reddit)
-                <br />
-                <b>- Mt. Silver Battle Background: </b>
-                - Mt. Silver Battle Background:
-                <a class="link" href="https://www.deviantart.com/serain">Serian</a>
-            </p>
-            <p>Original artist/creator of image(s), please DM for removal or credit.</p>
         </div>
-        <hr class="divider" />
-        <h1 class="text-center font-pkmEmerald text-[32px] uppercase">Contact</h1>
-        <p class="font-pkmEmerald text-[20px]">
-            If you have any questions/feedback/bug reports feel free to contact me on the email
-            below: <br />
-            &#8594;
-            <a class="link ml-2 text-[24px] font-bold" href="mailto: pokedle.contact@gmail.com">
-                pokedle.contact@gmail.com
-            </a>
-        </p>
     </BaseDialog>
 </template>
 
