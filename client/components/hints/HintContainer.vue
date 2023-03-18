9954a260564385d7bc7989e082beccb494b91467
Gabriel Haugbøl <grh@it-minds.dk>
2023-03-19 00:02:52 +0100

    Refactor Hint Container To Be More Generic

--- client/components/hints/HintContainer.vue
+++ client/components/hints/HintContainer.vue
@@ -3,9 +3,9 @@
         class="card min-w-[80px] max-w-[400px] flex-col font-pkmEmerald sm:max-w-[330px] sm:text-sm"
         :class="isShowHints ? 'w-[400px]' : ''"
     >
-        <p v-if="!isHintFlavortextUnlocked" class="gap-2 p-2 px-3 sm:p-1 sm:px-2 sm:pt-2">
+        <p v-if="!isHintOneUnlocked" class="gap-2 p-2 px-3 sm:p-1 sm:px-2 sm:pt-2">
             Guess
-            <b class="text-light-orange dark:text-dark-orange"> {{ guessesRemainingForHintFlavorText }} </b>
+            <b class="text-light-orange dark:text-dark-orange"> {{ guessesRemainingForHintOne }} </b>
             more times for a HINT
         </p>
         <div
@@ -13,7 +13,7 @@
             class="flex w-full flex-row items-center justify-between gap-2 p-2 px-3 pb-1 sm:pb-1 sm:pt-2"
         >
             <div class="flex flex-row text-base">
-                <p>HINT{{ isHintCardOneUnlocked ? 'S' : '' }}</p>
+                <p>HINT{{ isHintTwoUnlocked ? 'S' : '' }}</p>
             </div>
             <div>
                 <button class="pi pi-plus" @click="isShowHints = true"></button>
@@ -24,28 +24,28 @@
                 <div
                     @click="selectHintFlavortext"
                     class="hint-tab-header"
-                    :class="isShowHintFlavortext ? 'hint-tab-header-active' : ''"
+                    :class="isShowHintOne ? 'hint-tab-header-active' : ''"
                 >
                     Flavortext
                 </div>
                 <div
                     @click="selectHintCardOne"
                     class="hint-tab-header flex gap-1"
-                    :class="isShowHintCardOne ? 'hint-tab-header-active' : ''"
+                    :class="isShowHintTwo ? 'hint-tab-header-active' : ''"
                 >
                     Card Reveal 1
                     <div>
-                        <span :class="isHintCardOneUnlocked ? 'pi pi-lock-open' : 'pi pi-lock'"></span>
+                        <span :class="isHintTwoUnlocked ? 'pi pi-lock-open' : 'pi pi-lock'"></span>
                     </div>
                 </div>
                 <div
                     @click="selectHintCardTwo"
                     class="hint-tab-header flex gap-1"
-                    :class="isShowHintCardTwo ? 'hint-tab-header-active' : ''"
+                    :class="isShowHintThree ? 'hint-tab-header-active' : ''"
                 >
                     Card Reveal 2
                     <div>
-                        <span :class="isHintCardTwoUnlocked ? 'pi pi-lock-open' : 'pi pi-lock'"></span>
+                        <span :class="isHintThreeUnlocked ? 'pi pi-lock-open' : 'pi pi-lock'"></span>
                     </div>
                 </div>
             </div>
@@ -54,11 +54,11 @@
             </div>
         </div>
         <div v-if="isShowHints" class="w-full bg-gray-100 p-4 pt-3 text-justify dark:bg-zinc-700">
-            <div v-if="isShowHintFlavortext">
+            <div v-if="isShowHintOne">
                 {{ text }}
             </div>
-            <div v-else-if="isShowHintCardOne">CARD REVEAL 1</div>
-            <div v-else-if="isShowHintCardTwo">CARD REVEAL 2</div>
+            <div v-else-if="isShowHintTwo">CARD REVEAL 1</div>
+            <div v-else-if="isShowHintThree">CARD REVEAL 2</div>
         </div>
     </div>
 </template>
@@ -78,27 +78,27 @@ const hints = ['flavortext', 'card_reveal_1', 'card_reveal_2'];
 const selectedHint = ref('flavortext');
 const isShowHints = ref(false);
 
-const guessesRemainingForHintFlavorText = computed(() => 4- props.numberOfGuesses);
-const isHintFlavortextUnlocked = computed(() => (4 - props.numberOfGuesses) <= 0);
-const isHintCardOneUnlocked = computed(() => (6 - props.numberOfGuesses) <= 0);
-const isHintCardTwoUnlocked = computed(() => (8 - props.numberOfGuesses) <= 0);
+const guessesRemainingForHintOne = computed(() => 4 - props.numberOfGuesses);
+const isHintOneUnlocked = computed(() => (4 - props.numberOfGuesses) <= 0);
+const isHintTwoUnlocked = computed(() => (6 - props.numberOfGuesses) <= 0);
+const isHintThreeUnlocked = computed(() => (8 - props.numberOfGuesses) <= 0);
 
-const isShowHintFlavortext = computed(() => selectedHint.value === hints[0]);
-const isShowHintCardOne = computed(() => selectedHint.value === hints[1]);
-const isShowHintCardTwo = computed(() => selectedHint.value === hints[2]);
+const isShowHintOne = computed(() => selectedHint.value === hints[0]);
+const isShowHintTwo = computed(() => selectedHint.value === hints[1]);
+const isShowHintThree = computed(() => selectedHint.value === hints[2]);
 
 const selectHintFlavortext = () => {
-    if(isHintFlavortextUnlocked.value) {
+    if(isHintOneUnlocked.value) {
         selectedHint.value = hints[0]
     }
 }
 const selectHintCardOne = () => {
-    if(isHintCardOneUnlocked.value) {
+    if(isHintTwoUnlocked.value) {
         selectedHint.value = hints[1]
     }
 }
 const selectHintCardTwo = () => {
-    if(isHintCardTwoUnlocked.value) {
+    if(isHintThreeUnlocked.value) {
         selectedHint.value = hints[2]
     }
 }
