f03564a9847b1f87efce38cbf7d4275b4cffaadb
Gabriel Haugbøl <grh@it-minds.dk>
2023-03-23 12:38:18 +0100

    Add SingleResultHeader

---
+++ client/components/result/SingleResultHeader.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="single-result-container mb-1 items-center justify-center">
+        <span
+            class="border-b-2 border-light-border text-center font-pkmEmerald text-lg font-bold dark:border-dark-border"
+            >{{ headerText }}
+        </span>
+    </div>
+</template>
+
+<script setup>
+const props = defineProps({
+    headerText: String,
+});
+</script>
