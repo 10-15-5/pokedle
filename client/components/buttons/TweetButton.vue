25d1d1655e0d9b9ab8fbeba7c01f8b7214b793c5
Gabriel Haugbøl <grh@it-minds.dk>
2023-03-25 13:25:59 +0100

    Fix Style On Tweet BTN

--- client/components/buttons/TweetButton.vue
+++ client/components/buttons/TweetButton.vue
@@ -4,7 +4,7 @@
             class="card cursor-pointer gap-1 !bg-twitterblue p-1 px-2 text-white transition-all duration-100 ease-linear hover:scale-110 sm:py-0"
         >
             <SvgIcon type="mdi" :path="mdiTwitter" class="w-[24px] sm:w-[20px]" />
-            <p class="pt-1 font-pkmEmerald text-sm sm:text-xs sm:font-bold">SHARE RESULTS!</p>
+            <p class="pt-1 sm:pt-[5px] font-pkmEmerald text-sm sm:text-xs sm:font-bold">SHARE RESULTS!</p>
         </div>
     </a>
 </template>
