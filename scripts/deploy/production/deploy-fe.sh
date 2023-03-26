5f138fd267248480343d59e65f3c3aa5f2a5ccf7
Gabriel Haugbøl <grh@it-minds.dk>
2023-03-26 18:01:55 +0200

    Add Single Deployment Scripts For Prod FE and BE

--- scripts/deploy/production/deploy-fe.sh
+++ scripts/deploy/production/deploy-fe.sh
@@ -1,9 +1,26 @@
 #!/bin/zsh
-echo "$PWD"
-docker build -t gabr0236/pokedle-client -f docker/production/vue-client .
 
-docker tag gabr0236/pokedle-client \europe-west1-docker.pkg.dev/encoded-might-376114/pokedle-repo/pokedle-client
+# Require tag param
+if [ $# -lt 1 ]; then
+  echo "Error: Missing image tag. Usage: $1 IMAGE_TAG"
+  exit 1
+fi
 
-docker push europe-west1-docker.pkg.dev/encoded-might-376114/pokedle-repo/pokedle-client
+# Set variables
+IMAGE_NAME=pokedle-client
+DEPLOYMENT_NAME=pokedle-client-temp
+CONTAINER_NAME=pokedle-client-sha256-1
+IMAGE_PATH=docker/production/vue-client
+IMAGE_TAG=$1
 
-echo "Building is complete"
+# Build Docker image locally
+docker build -t gabr0236/$IMAGE_NAME -f $IMAGE_PATH .
+
+# Tag image
+docker tag gabr0236/$IMAGE_NAME \europe-west1-docker.pkg.dev/encoded-might-376114/pokedle-repo/$IMAGE_NAME:$IMAGE_TAG
+
+# Push Docker image to GCP Artifact Registry
+docker push europe-west1-docker.pkg.dev/encoded-might-376114/pokedle-repo/$IMAGE_NAME:$IMAGE_TAG
+
+# Update Deployment To Use New Image
+kubectl set image deployment/$DEPLOYMENT_NAME $CONTAINER_NAME=europe-west1-docker.pkg.dev/encoded-might-376114/pokedle-repo/$IMAGE_NAME:$IMAGE_TAG
