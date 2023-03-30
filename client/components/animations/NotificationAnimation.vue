9769dd32ae30a84b6f4590e407d2438b362052e0
Gabriel Haugbøl <grh@it-minds.dk>
2023-03-30 18:01:37 +0200

    Add NotificationAnimation To Statistics Tab

--- client/components/animations/NotificationAnimation.vue
+++ client/components/animations/NotificationAnimation.vue
@@ -1,7 +1,7 @@
 <template>
     <div
         v-if="!isClicked && isShowNotification"
-        class="absolute right-0 top-0 mt-[-33px] mr-[-13px] sm:mt-[-27px] sm:mr-[-8px] "
+        class="absolute right-0 top-0 mt-[-33px] mr-[-13px] sm:mt-[-27px] sm:mr-[-8px]"
     >
         <span class="relative flex h-10 w-10 sm:h-8 sm:w-8 items-center justify-center">
             <span class="absolute w-5 h-5 bg-red-500 opacity-0 rounded-full cursor-pointer" @click="disable"></span>
--- client/components/dialogs/StatisticsDialog.vue
+++ client/components/dialogs/StatisticsDialog.vue
@@ -1,12 +1,17 @@
 <template>
     <BaseDialog :isVisible="isVisible" :onClose="onClose">
-        <h1 class="text-center font-pkmEmerald text-[32px] uppercase">STATISTICS</h1>
-        <p class="text-center font-pkmEmerald text-[20px]">Coming Soon....</p>
+        <div class="flex flex-col justify-center items-center relative">
+            <h1 class="text-center font-pkmEmerald text-[32px] uppercase">STATISTICS</h1>
+            <span class="w-fit text-center font-pkmEmerald text-[20px]"
+                >Coming Soon.... <NotificationAnimation class="!mt-[36px] !mr-[165px]" />
+            </span>
+        </div>
     </BaseDialog>
 </template>
 
 <script setup>
 import BaseDialog from './BaseDialog.vue';
+import NotificationAnimation from '../animations/NotificationAnimation.vue';
 
 const props = defineProps({
     isVisible: Boolean,
