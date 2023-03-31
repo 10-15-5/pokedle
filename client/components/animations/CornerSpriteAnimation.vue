3a9fb7780c3c0e848eea8eac0bd0bee8919f3d29
Gabriel Haugbøl <grh@it-minds.dk>
2023-03-31 12:46:59 +0200

    Fix Not Possible To Remove Sprite Animation On Click

--- client/components/animations/CornerSpriteAnimation.vue
+++ client/components/animations/CornerSpriteAnimation.vue
@@ -3,7 +3,7 @@
         v-if="!isClicked && isShowNotification"
         class="absolute right-0 top-0 mt-[-33px] mr-[-13px] sm:mt-[-27px] sm:mr-[-8px]"
     >
-        <SpriteAnimation :isShowNotification="isShowNotification" />
+        <SpriteAnimation :isShowNotification="isShowNotification" @click="disable" />
     </div>
 </template>
 
